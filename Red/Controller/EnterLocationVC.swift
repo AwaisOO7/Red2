434e3f3a25fdb8a8a1d19e740ea5d86741620a57
master <master@masters-macbook-pro.local>
2018-09-10 16:51:26 +0500

    Final User UI Updated

---
+++ Red/Controller/EnterLocationVC.swift
@@ -0,0 +1,39 @@
+//
+//  EnterLocationVC.swift
+//  Red
+//
+//  Created by master on 9/8/18.
+//  Copyright © 2018 XcoderZ. All rights reserved.
+//
+
+import UIKit
+
+class EnterLocationVC: UIViewController {
+
+    @IBAction func OrderNowBtn(_ sender: UIButton) {
+    }
+    @IBAction func NextBtn(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Red/Controller/LocationMapVC.swift
+++ Red/Controller/LocationMapVC.swift
@@ -11,6 +11,52 @@ import GoogleMaps
 
 class LocationMapVC: UIViewController , GMSMapViewDelegate{
 
+    //IBOutlets of buttons and popup views
+    
+    @IBOutlet weak var PromoCodeTxt: UITextField!
+    @IBOutlet weak var WorkshopDestinationTxt: UITextField!
+    @IBOutlet weak var UserRemarks: UITextField!
+    @IBOutlet weak var CustomerLocationTxt: UITextField!
+    
+    @IBOutlet weak var DistanceANDCostView: ViewStyle!
+    @IBOutlet weak var AfterPromoCostLbl: UILabel!
+    @IBOutlet weak var TotalCostLbl: UILabel!
+    @IBOutlet weak var DistanceLabel: UILabel!
+    @IBOutlet weak var TermsAndConditions: UIButton!
+    @IBOutlet weak var OrderNowBtn: UIButtonStyle!
+    @IBOutlet weak var NextBTN: UIButtonStyle!
+    @IBOutlet weak var NavBarLabel: UILabel!
+    
+    //promo view outlets and functions
+    @IBOutlet weak var PromoCodeView: UIView!
+    @IBOutlet weak var PromoCodeLabel: UILabel!
+    
+    @IBOutlet weak var PromoDescriptionLBL: UILabel!
+    
+    @IBAction func CancelPromoView(_ sender: UIButton) {
+        self.PromoCodeView.isHidden = true
+        self.NextBTN.isHidden = false
+    }
+    @IBAction func OKPromoCodeBtn(_ sender: UIButton) {
+        self.DistanceANDCostView.isHidden = false
+        self.TermsAndConditions.isHidden = false
+        self.OrderNowBtn.isHidden = false
+        self.PromoCodeView.isHidden = true
+        NavBarLabel.text = "Towing (Accident)"
+    }
+    
+    
+    //Order View Outlets and functions
+    @IBOutlet weak var ConfirmOrderView: UIView!
+    
+    @IBAction func CancelConfirmOrderView(_ sender: UIButton) {
+        self.ConfirmOrderView.isHidden = true
+    }
+    
+    
+    
+    
+    
     @IBOutlet weak var viewMap: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,20 +65,24 @@ class LocationMapVC: UIViewController , GMSMapViewDelegate{
         viewMap.camera = camera
     }
 
+    @IBAction func OrderNowFunc(_ sender: UIButton) {
+        self.ConfirmOrderView.isHidden = false
+    }
+    @IBAction func NextFunc(_ sender: UIButton) {
+        self.NextBTN.isHidden = true
+        self.PromoCodeView.isHidden = false
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
+    
 
 }
